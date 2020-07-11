abcb22c807c395890e645bfefa5a69c90d551300
jsarthak <jsarthak@outlook.com>
2020-07-11 14:30:06 +0530

    add min and max forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,14 +4,13 @@ const forecast = (latitude, longitude, callback) => {
 
     const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=4e9927d082f8d0b899d490302f33e7f8`;
 
-    request({  url, json: true }, (error, {body}) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service.', undefined);
         } else if (body.message) {
             callback('Unable to find location', undefined);
-        }
-        else {
-            callback(undefined, `Current temperature is ${body.main.temp}`);
+        } else {
+            callback(undefined, `Current temperature is ${body.main.temp}, Min: ${body.main.temp_min}, Max: ${body.main.temp_max}`);
 
         }
     });
